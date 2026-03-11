ff49d0892bb9ea699b5361e6aab84fc26c9bfcfa
Brenner Souza <brenner.souza@gmail.com>
2026-03-11 20:13:56 -0300

    Criação Classe ChatResponse

--- src/modelo/ChatResponse.java
+++ src/modelo/ChatResponse.java
@@ -1,5 +1,42 @@
 package modelo;
 
 public class ChatResponse {
+    private String text;
+    private  String sourceId;
+    private String sessionId;
 
+    //CONSTRUCTORS
+    public ChatResponse() {}
+
+    public ChatResponse(String text, String sourceId, String sessionId) {
+        this.text = text;
+        this.sourceId = sourceId;
+        this.sessionId = sessionId;
+    }
+
+    //METODS
+
+    public String getText() {
+        return text;
+    }
+
+    public String getSourceId() {
+        return sourceId;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setSourceId(String sourceId) {
+        this.sourceId = sourceId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
 }
