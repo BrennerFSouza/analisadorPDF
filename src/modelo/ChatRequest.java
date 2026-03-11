f7a10a8cedc4b6f9826bcdada8cfac2959c46200
Brenner Souza <brenner.souza@gmail.com>
2026-03-11 19:39:49 -0300

    Criação classe ChatRequest

--- src/modelo/ChatRequest.java
+++ src/modelo/ChatRequest.java
@@ -1,4 +1,25 @@
 package modelo;
 
 public class ChatRequest {
+    private String sourceId;
+    private Message[] messages;
+
+    //CONSTRUCTOR
+
+    public ChatRequest(){}
+
+    public ChatRequest(String sourceId, Message[] messages){
+        this.sourceId = sourceId;
+        this.messages = messages;
+    }
+
+    //METODS
+
+    public String getSourceId() {
+        return sourceId;
+    }
+
+    public Message[] getMessages() {
+        return messages;
+    }
 }
