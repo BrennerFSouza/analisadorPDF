af772c9be1a76497bb549f38a6a125d7910c097f
Brenner Souza <brenner.souza@gmail.com>
2026-03-11 21:37:53 -0300

    Criação da classe ChatPdfService

--- src/cli/Main.java
+++ src/cli/Main.java
@@ -3,13 +3,5 @@ import modelo.Message;
 
 void main() {
 
-    String apiKey = ApiConfig.obterApiKey();
-    System.out.println(apiKey);
-
-    var message = new Message("Teste rule", "Teste content");
-
-    System.out.println(message.getRole());
-    System.out.println(message.getContent());
-
 
 }
--- src/servico/ChatPdfService.java
+++ src/servico/ChatPdfService.java
@@ -1,4 +1,56 @@
 package servico;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import configuracao.ApiConfig;
+import modelo.ChatRequest;
+import modelo.ChatResponse;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import static java.net.http.HttpRequest.BodyPublishers.ofString;
+import static java.net.http.HttpResponse.BodyHandlers.ofString;
 
 public class ChatPdfService {
+    private final HttpClient client;
+
+    private final Gson gson;
+
+    private final String apiKey;
+
+
+    //CONSTRUCTOR
+    public ChatPdfService(HttpClient client, Gson gson, String apiKey) {
+        this.client = client;
+        this.gson = gson;
+        this.apiKey = apiKey;
+    }
+
+    //METODS
+    public ChatResponse enviarPergunta(ChatRequest request){
+        try{
+            String jsonRequest = gson.toJson(request);
+
+            HttpRequest httpRequest = HttpRequest.newBuilder()
+                    .uri(URI.create(ApiConfig.URL_BASE + "/chats/message"))
+                    .header("x-api-key", apiKey)
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(jsonRequest))
+                    .build();
+
+            HttpResponse<String> response = client.send(httpRequest,
+                    HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() != 200){
+                System.err.println("❌ API ERRO " + response.statusCode());
+                return null;
+            }
+
+            return gson.fromJson(response.body(), ChatResponse.class);
+        }catch (Exception e){
+            System.err.println("❌ ERRO: " + e.getMessage());
+            return null;
+        }
+    }
 }
