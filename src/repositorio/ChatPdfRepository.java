b5241ebc0c90e7b9df26bc1f041e2c1c7022464c
Brenner Souza <brenner.souza@gmail.com>
2026-03-25 17:11:34 -0300

    Criando ChatPdfRespository

--- src/repositorio/ChatPdfRepository.java
+++ src/repositorio/ChatPdfRepository.java
@@ -1,4 +1,47 @@
 package repositorio;
 
+import com.google.gson.Gson;
+import configuracao.ApiConfig;
+import modelo.ChatRequest;
+import modelo.ChatResponse;
+
+import java.net.http.HttpClient;
+
 public class ChatPdfRepository {
+    private final HttpClient client;
+    private final Gson gson = new Gson();
+    private final String apiKey = ApiConfig.obterApiKey();
+
+    public ChatPdfRepository() {
+        this.client = client;
+    }
+
+    public ChatResponse enviarPergunta(ChatRequest request) {
+        try {
+            String jsonRequest = gson.toJson(request);
+
+            HttpRequest httpRequest = HttpRequest.newBuilder()
+                    .uri(URI.create(ApiConfig.URL_BASE + "/chats/message"))
+                    .header("x-api-key", apiKey)
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(jsonRequest))
+                    .build();
+
+            HttpResponse<String> response = client.send(httpRequest,
+                    HttpResponse.BodyHandlers.ofString());
+
+
+            if (response.statusCode() != 200) {
+                System.err.println("❌ API ERRO " + response.statusCode());
+                return null;
+            }
+
+            return gson.fromJson(response.body(), ChatResponse.class);
+        } catch (Exception e) {
+            System.err.println("❌ ERRO: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public
 }
--- src/servico/ChatPdfService.java
+++ src/servico/ChatPdfService.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import configuracao.ApiConfig;
 import modelo.ChatRequest;
 import modelo.ChatResponse;
+import repositorio.ChatPdfRepository;
 
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -11,44 +12,14 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ChatPdfService {
-    private final HttpClient client;
+    private ChatPdfRepository chatPdfRepository = new ChatPdfRepository();
 
-    private final Gson gson;
-
-    private final String apiKey;
 
     //CONSTRUCTOR
-    public ChatPdfService() {
-        this.client = HttpClient.newHttpClient();
-        this.gson = new Gson();
-        this.apiKey = ApiConfig.obterApiKey();
-    }
+
 
     //METODS
     public ChatResponse enviarPergunta(ChatRequest request) {
-        try {
-            String jsonRequest = gson.toJson(request);
-
-            HttpRequest httpRequest = HttpRequest.newBuilder()
-                    .uri(URI.create(ApiConfig.URL_BASE + "/chats/message"))
-                    .header("x-api-key", apiKey)
-                    .header("Content-Type", "application/json")
-                    .POST(HttpRequest.BodyPublishers.ofString(jsonRequest))
-                    .build();
-
-            HttpResponse<String> response = client.send(httpRequest,
-                    HttpResponse.BodyHandlers.ofString());
-
-
-            if (response.statusCode() != 200) {
-                System.err.println("❌ API ERRO " + response.statusCode());
-                return null;
-            }
-
-            return gson.fromJson(response.body(), ChatResponse.class);
-        } catch (Exception e) {
-            System.err.println("❌ ERRO: " + e.getMessage());
-            return null;
-        }
+        return chatPdfRepository.enviarPergunta(request);
     }
 }
